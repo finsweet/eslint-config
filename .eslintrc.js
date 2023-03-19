36cbfb0eaf48739831bf2eea376c6183fc902827
Alex Iglesias <alexiglesias@finsweet.com>
2023-03-19 18:04:15 +0100

    fix missing prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    sourceType: 'module', // Allows for the use of imports
   },
   extends: [
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "plugin:prettier/recommended",
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended',
   ],
 
-  plugins: ["simple-import-sort"],
+  plugins: ['simple-import-sort'],
 
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        endOfLine: "auto",
+        endOfLine: 'auto',
       },
     ],
 
     // import sorting
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
 
     // disallow else after a return in an if
-    "no-else-return": ["error", { allowElseIf: false }],
+    'no-else-return': ['error', { allowElseIf: false }],
 
     // disallow use of assignment in return statement
-    "no-return-assign": ["error", "always"],
+    'no-return-assign': ['error', 'always'],
 
     // Prefer destructuring from arrays and objects
-    "prefer-destructuring": [
-      "error",
+    'prefer-destructuring': [
+      'error',
       {
         VariableDeclarator: {
           array: false,
@@ -48,17 +48,17 @@ module.exports = {
     ],
 
     // disallow unnecessary constructor
-    "no-useless-constructor": "error",
+    'no-useless-constructor': 'error',
 
     // disallow use of console statements (except for console.error)
-    "no-console": ["warn", { allow: ["error"] }],
+    'no-console': ['warn', { allow: ['error'] }],
 
     // disallow the unary operators ++ and --
-    "no-plusplus": ["warn", { allowForLoopAfterthoughts: true }],
+    'no-plusplus': ['warn', { allowForLoopAfterthoughts: true }],
 
     // require the use of === and !==
-    eqeqeq: ["error", "always", { null: "ignore" }],
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
   },
 
-  ignorePatterns: ["dist/", ".eslintrc.js"], // Avoid linting on the dist folder and, ironically, this file too
+  ignorePatterns: ['dist/', '.eslintrc.js'], // Avoid linting on the dist folder and, ironically, this file too
 };
